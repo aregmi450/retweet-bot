d391ea81004f5b42be854753fcb5a2fcfdbfe778
Arun Regmi <46434570+aregmi450@users.noreply.github.com>
2021-04-26 06:53:38 +0545

    Update retweet.py

--- retweet.py
+++ retweet.py
@@ -4,21 +4,21 @@ from time import sleep
 # Make sure your keys.py file lives in the same directory as this .py file.
 
 
-auth = tweepy.OAuthHandler('Insert your  auth codes here both token and secret')
-auth.set_access_token('insert access token code', 'insert access token secret')
+auth = tweepy.OAuthHandler('SZg2GdKeb0G0BVj5dSujfzM94', '4xXr6eZRDqqBgGrNHfu7V4uyRztPZoD1rBM44zBuiyTvQ13kge')
+auth.set_access_token('1278599604947173377-OxL2W1NTvGR7LQtT5zk0kmDR0L3lFx', '8jm4C7aoHsuJaQ4mpjlsEMPzGBZr3ilD4WJdYpEP9TTGB')
 api = tweepy.API(auth)
-for tweet in tweepy.Cursor(api.search, q='#keyword content you want to retweet').items(999):
+for tweet in tweepy.Cursor(api.search, q='#COVID19India').items(9999):
     try:
-        print('\n Username found tweet by @' +
+        print('\nClancy found tweet by @' +
               tweet.user.screen_name + '. ' + 'Attempting to retweet.')
 
         tweet.retweet()
         print('Retweet published successfully.')
-        # no of times the bot should search for hashtags of the keyword is written inside items
+
         # Where sleep(10), sleep is measured in seconds.
         # Change 10 to amount of seconds you want to have in-between retweets.
         # Read Twitter's rules on automation. Don't spam!
-        sleep(200)
+        sleep(120)
 
     # Some basic error handling. Will print out why retweet failed, into your terminal.
     except tweepy.TweepError as error:
